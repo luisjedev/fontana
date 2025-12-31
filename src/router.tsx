8d9aa477e70b7b331b6dcdf8cc3b110d0b93ef40
luisjedev <luisjedev@gmail.com>
2025-12-31 12:41:21 +0100

    refactor: elimina comentarios redundantes del enrutador y añade la regla `css.lint.unknownAtRules` a la configuración de VS Code.

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,8 @@
 import { createRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import * as TanstackQuery from "./shared/config/tanstack-query/root-provider";
 
-// Create a new router instance
 export const getRouter = () => {
 	const rqContext = TanstackQuery.getContext();
 
